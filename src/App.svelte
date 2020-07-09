8c460c3d420de24ad598f831797a7a5be4bd7abe
Oliver Read <oliver@oliverread.co.uk>
2020-07-09 15:52:21 +0100

    Set up prettier

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,44 @@
 <script>
-	import NavBar from "./components/NavBar.svelte";
-	import TailwindStyles from "./components/TailwindStyles.svelte";
+  import NavBar from "./components/NavBar.svelte";
+  import TailwindStyles from "./components/TailwindStyles.svelte";
 
-	export let name;
+  export let name;
 </script>
+
+<style>
+  .test {
+    @apply bg-orange-500;
+  }
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
 
 <NavBar />
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<p class="bg-green-500">Testing 123</p>
-	<p class="test">Testing again</p>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+  <p class="bg-green-500">Testing 123</p>
+  <p class="test">Testing again</p>
 </main>
-
-<style>
-	.test {
-		@apply bg-orange-500;
-	}
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
